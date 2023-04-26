6943b141f28f853d1dc5d4481971039c61bfceef
Alejandro Veiga López <kaos1323@gmail.com>
2023-04-26 15:29:52 +0200

    WIP: Tutorial 1:34:41

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+
 @NgModule({
 
-  exports: [MatToolbarModule, MatCardModule]
+  exports: [MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule]
 
 })
 
